b37f5f5f66be49908209a18f76201667f25e2358
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-17 12:11:37 +0300

    task-01: fix listeners onClose and onShow

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,48 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
-// console.log(galleryEl);
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const markup = galleryItems
   .map((item) => {
     const { preview, original, description } = item;
     return `<li class="gallery__item">
-            <a class="gallery__link" href=${original}>
-              <img
-                class="gallery__image"
-                src=${preview}
-                data-source=${original}
-                alt=${description}
-              />
-            </a>
-          </li>`;
+              <a class="gallery__link" href=${original}>
+                <img
+                  class="gallery__image"
+                  src=${preview}
+                  data-source=${original}
+                  alt=${description}
+                />
+              </a>
+            </li>`;
   })
   .join("");
 
 galleryEl.insertAdjacentHTML("beforeend", markup);
 
+let instance = {};
+
 function onImgClick(e) {
   e.preventDefault();
-  //
-  console.log(e);
-  console.dir(e.target);
-  console.log(e.target.dataset.source);
+  const source = e.target.dataset.source;
+
+  instance = basicLightbox.create(`<img src=${source}>`, {
+    onShow: () => document.addEventListener("keydown", onEscPress),
+    onClose: () => document.removeEventListener("keydown", onEscPress),
+  });
+  // instance.show(() => document.addEventListener("keydown", onEscPress));
+  instance.show();
+}
+
+function onEscPress(e) {
+  console.log(e.code); // to check is listener present
+
+  if (e.code === "Escape") {
+    // instance.close(() => document.removeEventListener("keydown", onEscPress));
+    instance.close();
+  }
 }
 
 galleryEl.addEventListener("click", onImgClick);
