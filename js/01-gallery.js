d808adb45c999ea55e664030a11a5c9e9b7b32ea
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-17 11:01:32 +0300

    task-01: gallery markup &  basicLightbox cdn

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+// console.log(galleryEl);
 
 console.log(galleryItems);
+
+const markup = galleryItems
+  .map((item) => {
+    const { preview, original, description } = item;
+    return `<li class="gallery__item">
+            <a class="gallery__link" href=${original}>
+              <img
+                class="gallery__image"
+                src=${preview}
+                data-source=${original}
+                alt=${description}
+              />
+            </a>
+          </li>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+function onImgClick(e) {
+  e.preventDefault();
+  //
+  console.log(e);
+  console.dir(e.target);
+  console.log(e.target.dataset.source);
+}
+
+galleryEl.addEventListener("click", onImgClick);
+// ${}
