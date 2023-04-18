265d259fbb5c9e42114ed5518cee192d506c00fb
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-19 00:47:12 +0300

    task-02: SimpleLightbox added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,22 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
-// ${}
+const markup = galleryItems
+  .map((item) => {
+    const { preview, original, description } = item;
+    return `<li class="gallery__item">
+              <a class="gallery__link" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+              </a>
+            </li>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.8,
+  captionsData: "alt",
+  captionDelay: 250,
+});
