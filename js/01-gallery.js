bdf9a7dce5316bed5c5160603985b55999104463
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-19 21:40:33 +0300

    refactor to createGalleryMarkup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,11 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
 
-const markup = galleryItems
-  .map((item) => {
-    const { preview, original, description } = item;
-    return `<li class="gallery__item">
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      // const { preview, original, description } = item;
+      return `<li class="gallery__item">
               <a class="gallery__link" href=${original}>
                 <img
                   class="gallery__image"
@@ -15,16 +16,22 @@ const markup = galleryItems
                 />
               </a>
             </li>`;
-  })
-  .join("");
+    })
+    .join("");
+}
 
-galleryEl.insertAdjacentHTML("beforeend", markup);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
 let instance = {};
 
 function onImgClick(e) {
   e.preventDefault();
-  if (e.target.nodeName !== "IMG") return;
+
+  // const isImage = e.target.nodeName === "IMG";
+  const isImage = e.target.classList.contains("gallery__image");
+  if (!isImage) return;
 
   const source = e.target.dataset.source;
   instance = createInstance(source);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,18 +2,23 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
 
-const markup = galleryItems
-  .map((item) => {
-    const { preview, original, description } = item;
-    return `<li class="gallery__item">
+function createGalleryMarkup(items) {
+  //
+  return items
+    .map(({ preview, original, description }) => {
+      // const { preview, original, description } = item;
+      return `<li class="gallery__item">
               <a class="gallery__link" href=${original}>
                 <img class="gallery__image" src=${preview} alt=${description} />
               </a>
             </li>`;
-  })
-  .join("");
+    })
+    .join("");
+}
 
-galleryEl.insertAdjacentHTML("beforeend", markup);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
 new SimpleLightbox(".gallery a", {
   overlayOpacity: 0.8,
